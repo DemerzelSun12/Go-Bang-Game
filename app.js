e616e011b16158e72f3376f346eb04b2ea33f0c7
DemerzelSun12 <DemerzelSun@outlook.com>
2021-03-29 22:09:25 +0800

    Change port to 8080

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const WebSocket = require('ws')
 const server = http.Server(app)
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
-server.listen(4444 , () => {
+server.listen(8080 , () => {
   console.log('服务器已开启');
 })
 
